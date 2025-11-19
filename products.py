38582d1140eeeebf843fc547bf109771ad04bb9e
Linhan Dang <17868741386@163.com>
2025-11-20 00:16:15 +0800

    add check file exist

--- products.py
+++ products.py
@@ -1,12 +1,18 @@
+import os                              
+                                       
 # 读取档案
-products = []
-with open('products.csv', 'r', encoding='utf-8')as f:
-	for line in f:
-		if '商品,价格' in line:  
-			continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(products)	
+products = []                          
+if os.path.isfile('products.csv'):      
+	print('找到档案了!')
+	with open('products.csv', 'r', encoding='utf-8')as f:
+		for line in f:
+			if '商品,价格' in line:  
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)	
+else:
+	print('找不到档案......')
 
 # 让使用者输入
 while True:
@@ -23,7 +29,7 @@ print(products)
 for p in products: 
 	print(p[0], '的价格是', p[1])
 
-# 写入档案
+# 写入档案	
 with open('products.csv', 'w', encoding='utf-8') as f:  
 	f.write('商品,价格\n')
 	for p in products:
